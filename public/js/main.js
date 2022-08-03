0b07934cc0718077577b894e435f5c604eb1d43b
TaiLe <Taic21a.th1@gmail.com>
2022-08-03 20:10:15 +0700

    Backup

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,59 @@
-
-function showCurrentTime() {
-    var time = new Date();
-    var currentHour = time.getHours();
-    var currentMinute = time.getMinutes();
-    var currentSecond = time.getSeconds();
-    if (currentHour >= 12) {
-        var currentTime = currentHour + ':' + currentMinute + ':' + currentSecond + ' PM' + '!';
-    } else {
-        var currentTime = currentHour + ':' + currentMinute + ':' + currentSecond + ' AM' + '!';
+const students = [
+  {
+    id: 1,
+    firstName: 'Lê Trần Tấn',
+    lastName: 'Tài',
+    birthDay: '12/01/2003',
+    isStudent: true,
+  },
+  {
+    id: 2,
+    firstName: 'Lê Trần Tấn',
+    lastName: 'Tài',
+    birthDay: '11/01/2003',
+    isStudent: false,
+  },
+  {
+    id: 3,
+    firstName: 'Lê Trần Tấn',
+    lastName: 'Tài',
+    birthDay: '11/01/2003',
+    isStudent: false,
+  },
+];
+// Hàm forEach2
+Array.prototype.forEach2 = function (callBack) {
+  if (typeof callBack === 'function') {
+    for (var index in this) {
+      if (this.hasOwnProperty(index)) {
+        callBack(this[index], index, this);
+      }
     }
-    return currentTime;
-}
-var getClockElement = document.querySelector(".clock");
-setInterval(function () {
-    getClockElement.innerText = showCurrentTime();
-}, 500)
-showCurrentTime();
+  }
+};
+
+//Hàm filter
+Array.prototype.myFilter = function (callBack) {
+  if (typeof callBack === 'function') {
+    let arr = [];
+    let index = 0;
+    for (var i in this) {
+      if (this.hasOwnProperty(i)) {
+        let result = callBack(this[i], i, this);
+        if (result) {
+          arr.push(this[i]);
+        }
+      }
+    }
+    return arr.length == 1 ? this[index] : arr;
+  }
+};
+
+const taile = students.myFilter((student) => {
+  return student.isStudent == true;
+});
+
+console.log(taile);
+// isStudent.forEach2((student) => {
+//   console.log(student);
+// });
